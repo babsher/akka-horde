e0f5a0f0844f8f2ab78a24585b6c97dbf4430a6b
bryan.absher <bryan.absher@lab49.com>
2015-02-05 17:37:25 -0500

    reformatted code

--- src/main/scala/edu/gmu/horde/actors/HttpService.scala
+++ src/main/scala/edu/gmu/horde/actors/HttpService.scala
@@ -1,37 +1,18 @@
 package edu.gmu.horde.actors
 
-import akka.actor._
+import akka.actor.{ActorSystem, _}
 import akka.event.{Logging, LoggingAdapter}
 import akka.http.Http
-import akka.http.client.RequestBuilding
-import akka.http.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.marshalling.ToResponseMarshallable
-import akka.http.model.{HttpResponse, HttpRequest}
-import akka.http.model.StatusCodes._
+import akka.http.server.Directives._
 import akka.pattern._
-import akka.util._
-import akka.http.server.Directives._
-import akka.http.server.directives._
-import com.typesafe.config.{ConfigFactory, Config}
-import akka.actor.ActorSystem
-import akka.event.{LoggingAdapter, Logging}
-import akka.http.Http
-import akka.http.client.RequestBuilding
-import akka.http.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.marshalling.ToResponseMarshallable
-import akka.http.model.{HttpResponse, HttpRequest}
-import akka.http.model.StatusCodes._
-import akka.http.server.Directives._
-import akka.http.unmarshalling.Unmarshal
 import akka.stream.FlowMaterializer
-import akka.stream.scaladsl.{Sink, Source}
-import com.typesafe.config.Config
-import com.typesafe.config.ConfigFactory
-import java.io.IOException
-import scala.concurrent.{ExecutionContextExecutor, Future}
-import scala.math._
-import scala.concurrent.duration._
+import akka.util._
+import com.typesafe.config.{Config, ConfigFactory}
 import spray.json.DefaultJsonProtocol
+
+import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.duration._
 
 trait Protocols extends DefaultJsonProtocol {
   implicit val agentInfoFormat = jsonFormat2(AgentInfo.apply)
@@ -40,26 +21,26 @@ trait Protocols extends DefaultJsonProtocol {
 
 trait ZergHordeService extends Protocols {
   implicit val system: ActorSystem
-  implicit def executor: ExecutionContextExecutor
   implicit val materializer: FlowMaterializer
   implicit val timeout: Timeout = Timeout(15 seconds)
-
-  def config: Config
   val logger: LoggingAdapter
   val horde: ActorRef
-
   val routes = {
     get {
       getFromDirectory("app")
     } ~
-    pathPrefix("api") {
-      pathPrefix("agents") {
-        get {
-          complete((horde ? RequestAgentInfo()).mapTo[ToResponseMarshallable])
+      pathPrefix("api") {
+        pathPrefix("agents") {
+          get {
+            complete((horde ? RequestAgentInfo()).mapTo[ToResponseMarshallable])
+          }
         }
       }
-    }
   }
+
+  implicit def executor: ExecutionContextExecutor
+
+  def config: Config
 }
 
 object ZergHordeService extends App with ZergHordeService {
