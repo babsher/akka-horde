e97636ce76ef8930a40ae994cfbe9e04d04cc119
Bryan Absher <bryan.absher@gmail.com>
2015-02-06 12:32:20 -0500

    added basic angular app

---
+++ src/main/resources/app/src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('app', ['ngAnimate', 'ui.router', 'ngMaterial'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
