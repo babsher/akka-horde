2d29700a781c424bf289b03b36d428028ff92fd9
Bryan Absher <bryan.absher@gmail.com>
2015-02-11 14:35:45 -0500

    added nav bar

--- src/main/resources/app/src/components/navbar/navbar.controller.js
+++ src/main/resources/app/src/components/navbar/navbar.controller.js
@@ -1,6 +1,19 @@
 'use strict';
 
 angular.module('app')
-  .controller('NavbarCtrl', function ($scope) {
-    $scope.date = new Date();
+  .controller('SidenavCtrl', function($scope, $mdSidenav, $log) {
+    $scope.toggleLeft = function() {
+      $mdSidenav('left').toggle()
+        .then(function(){
+          $log.debug("toggle left is done");
+        });
+    };
+  })
+  .controller('LeftCtrl', function($scope, $mdSidenav, $log) {
+    $scope.close = function() {
+      $mdSidenav('left').close()
+        .then(function(){
+          $log.debug("close LEFT is done");
+        });
+    };
   });
