6d7011df7481d93caa6b958f3e76516d116b28ff
bryan.absher <bryan.absher@lab49.com>
2015-02-13 12:06:32 -0500

    removed path alias

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-alias npm-exec='PATH=$(npm bin):$PATH'
 cd src/main/resources/app
 npm install
-npm-exec bower install
-npm-exec gulp build
+PATH=$(npm bin):$PATH bower install
+PATH=$(npm bin):$PATH gulp build
